d28916348bfdb285251b50f74470e8358c9f456a
Mirzipan <3118304+Mirzipan@users.noreply.github.com>
2020-10-17 13:54:06 +0200

    + added Logo * Quit button now quits

---
+++ Assets/Scripts/Utils/QuitApplication.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Utils
+{
+	public class QuitApplication : MonoBehaviour
+	{
+		public void Quit()
+		{
+#if UNITY_EDITOR
+			EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+		}
+	}
+}
